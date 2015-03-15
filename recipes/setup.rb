5682604fb375a258b380b35030501d23dfdb8a4a
Sunil Sayala <sunil.s@jobaline.com>
2015-03-15 16:55:01 -0700

    Added User creation (the Chef way of user creation doesnt work on opsworks)

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,1 +1,18 @@
 Chef::Log.info("EXECUTING setup")
+
+# Add User #
+execute 'create-user' do
+  Chef::Log.info("creating user...")
+  user = node['rabbitmq']['user']
+  pass = node['rabbitmq']['pass']
+  cmd = 'rabbitmqctl add_user %s %s && rabbitmq set_user_tages %s administrator && rabbit rabbitmqctl set_permissions -p / %s ".*" ".*" ".*"' % [user,pass,user,user]
+  command cmd
+  action :nothing
+end
+
+
+execute 'create-queues' do
+  Chef::Log.info("creating queues...")
+  
+end
+
